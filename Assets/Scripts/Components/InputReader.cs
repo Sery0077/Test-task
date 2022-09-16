f8cd885010f484078ba524d026e537eb952ce74c
Sery0077 <sery.vlasenko.ru@gmail.com>
2022-09-16 12:33:17 +0300

    add touch input

--- Assets/Scripts/Components/InputReader.cs
+++ Assets/Scripts/Components/InputReader.cs
@@ -1,6 +1,8 @@
 using Chars;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 namespace Components
 {
@@ -8,10 +10,29 @@ namespace Components
     {
         [SerializeField] private Player player;
 
+        void Start()
+        {
+            EnhancedTouchSupport.Enable();
+            TouchSimulation.Enable();
+        }
+
         public void OnMovement(InputAction.CallbackContext context)
         {
+            Debug.Log(Touchscreen.current.touches.Count);
             var direction = context.ReadValue<Vector2>();
             player.SetDirection(direction);
+        }
+
+        private void Update()
+        {
+            if (Touch.activeFingers.Count > 0)
+            {
+                player.SetDirection(Vector2.up);
+            }
+            else
+            {
+                player.SetDirection(Vector2.down);
+            }
         }
     }
 }
